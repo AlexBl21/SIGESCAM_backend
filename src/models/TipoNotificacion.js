749e98cd95eee267e9ab96e9b41ec1a46e2f8c6d
LizethDanielaV <lizehdanielavb@ufps.edu.co>
2025-04-18 15:55:44 -0500

    Correccion en tipo notificacion

--- src/models/TipoNotificacion.js
+++ src/models/TipoNotificacion.js
@@ -6,7 +6,7 @@ const TipoNotificacion = db.define("tipo_notificacion", {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     nombre: { type: DataTypes.STRING(20), allownull: false}, 
     descripcion: { type: DataTypes.STRING(100), allownull: false}, 
-    activo: { type: dataTypes.BOOLEAN, allowNull: false, defaultValue: true}
+    activo: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: true}
 }, {
     timestamps: false,
     freezeTableName: true
