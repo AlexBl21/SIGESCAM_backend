d41dba2ac679ec9ad33c059f84ecc114496083a2
LizethDanielaV <lizehdanielavb@ufps.edu.co>
2025-05-10 21:33:50 -0500

    Creacion endpoints en sugerencia cambiar estado y clasificacion

--- src/controllers/SugerenciaController.js
+++ src/controllers/SugerenciaController.js
@@ -34,8 +34,27 @@ async function listarRechazadas(req, res) {
         const sugerencias = await SugerenciaService.listarRechazadas();
         res.status(200).json(sugerencias);
     } catch (error) {
-        res.status(error.statusCode || 500).jsonn({ message: "Error al obtener sugerencias", error: error.message })
+        res.status(error.statusCode || 500).json({ message: "Error al obtener sugerencias", error: error.message })
     }
 };
 
-export default{registrar, listarNoClasificadas, listarAceptadas, listarRechazadas};
+async function cambiarEstado(req, res) {
+    try {
+        const {id_sugerencia} = req.params;
+        const sugerencia = await SugerenciaService.cambiarEstado(id_sugerencia, req.body.estado);
+        res.status(200).json(sugerencia);
+    } catch (error) {
+        res.status(error.statusCode || 500).json({ message: "Error al obtener sugerencias", error: error.message })
+    }
+};
+
+async function cambiarClasificacion(req, res) {
+    try {
+        const {id_sugerencia} = req.params;
+        const sugerencia = await SugerenciaService.cambiarClasificacion(id_sugerencia, req.body.clasificada);
+        res.status(200).json(sugerencia);
+    } catch (error) {
+        res.status(error.statusCode || 500).json({ message: "Error al obtener sugerencias", error: error.message })
+    }
+}
+export default{registrar, listarNoClasificadas, listarAceptadas, listarRechazadas, cambiarEstado, cambiarClasificacion};
--- src/db/db.js
+++ src/db/db.js
@@ -4,14 +4,14 @@ import dotenv from "dotenv";
 dotenv.config({
     path: "../.env"
 });
-/*
+
 const sequelize = new Sequelize(process.env.MYSQL_URL, {
     define: { timestamps: false }
 });
-*/
+/*
 const sequelize = new Sequelize("sigescam", "root", "", {
     host: "localhost",
     dialect: "mysql"
   });
-  
+  */
  export default sequelize;
