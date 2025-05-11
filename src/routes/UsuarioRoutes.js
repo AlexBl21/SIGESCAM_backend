ff3a0830b8f9ce715e8c29f6c19b31ff713d8ed8
LeydiD <leydialejaduran@gmail.com>
2025-05-11 00:13:55 -0500

    Se implemento la recuperacion de contraseña por SMS y email

--- src/routes/UsuarioRoutes.js
+++ src/routes/UsuarioRoutes.js
@@ -9,7 +9,7 @@ router.get("/", usuarioController.listar);
 router.put("/:dni", usuarioController.editar);
 router.patch("/:dni", usuarioController.cambioDeEstado);
 router.get("/:dni", usuarioController.buscarPorId);
-router.put("/:dni", editarCorreo);
+router.put("/correo/:dni", editarCorreo);
 router.get("/:dni", obtenerUsuarioPorDNI);
 
 export default router;
